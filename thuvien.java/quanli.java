f380eb0e5b2d4d40bb53a969eb1ddd663829366e
Hwang <whales.iluca@gmail.com>
2025-05-14 08:18:16 +0000

    h

--- thuvien.java/quanli.java
+++ thuvien.java/quanli.java
@@ -2,7 +2,7 @@ public class quanli {
     public static void main(String[] args) {
     System.out.println("Hello, World");
 
-    books books1 = new books("001", "KhoaPug", "Huy");
+    books b = new books("001", "KhoaPug", "Huy");
     System.out.println("bookid: " + " " + "author");
     }
 }
