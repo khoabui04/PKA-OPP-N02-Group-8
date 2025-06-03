6c7b495166ed3fcddee7d3840c1962402edc2708
Hwang <whales.iluca@gmail.com>
2025-06-03 06:14:38 +0000

    update

--- review/TestSequence.java
+++ review/TestSequence.java
@@ -1,23 +1,19 @@
 public class TestSequence {
     public static void main(String[] args) {
-        
         Sequence sequence = new Sequence(5);
-       
+
         sequence.add("Object 1");
         sequence.add("Object 2");
         sequence.add("Object 3");
-        
-        
+
         Selector selector = sequence.getSelector();
-        
-      
+
         System.out.println("Testing Selector:");
         while (!selector.end()) {
             System.out.println("Current: " + selector.current());
             selector.next();
         }
-        
-      
+
         System.out.println("End of sequence reached: " + selector.end());
     }
 }
