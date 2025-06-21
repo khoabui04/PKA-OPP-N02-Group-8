235d156e911b90b45d6ff40bbbf371fe16b7b6b7
Hwang <whales.iluca@gmail.com>
2025-06-21 07:29:13 +0000

    update

--- gs-serving-web-content-main/complete/src/main/java/com/example/library/service/BorrowingSlipService.java
+++ gs-serving-web-content-main/complete/src/main/java/com/example/library/service/BorrowingSlipService.java
@@ -3,7 +3,9 @@ package com.example.library.service;
 import com.example.library.model.BorrowingSlip;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BorrowingSlipService {
     public List<BorrowingSlip> getSlipsByBorrowerID(List<BorrowingSlip> slips, String borrowerID) {
         return slips.stream()
